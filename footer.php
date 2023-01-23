bbf536b6ad5474676f1c4e112f71c3c9f3ced30c
Rehan Khan <rehan.khan@thrasio.com>
2023-01-23 18:22:08 +0530

    footer updated

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'rehan-work' ), 'rehan-work', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'rehan-work' ), 'rehan-work', '<a href="http://rehan.work/">Rehan</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
